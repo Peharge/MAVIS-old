72f550524710aad5213077c281bc02d894cf2206
Peharge <Julian.kropff@gmx.de>
2024-11-15 18:38:26 +0100

    Update

---
+++ run-main.bat
@@ -0,0 +1,31 @@
+@echo off
+
+set PYTHON_PATH=C:\Users\julia\PycharmProjects\MAVIS\.venv\Scripts\python.exe
+
+set SCRIPT_PATH_1=C:\Users\julia\PycharmProjects\MAVIS\info.py
+set SCRIPT_PATH_2=C:\Users\julia\PycharmProjects\MAVIS\main16.py
+
+if not exist "%PYTHON_PATH%" (
+    echo Fehler: Python-Interpreter nicht gefunden: %PYTHON_PATH%
+    exit /B 1
+)
+
+if not exist "%SCRIPT_PATH_1%" (
+    echo Fehler: Skript nicht gefunden: %SCRIPT_PATH_1%
+    exit /B 1
+)
+
+echo Starte Skript: %SCRIPT_PATH_1%
+"%PYTHON_PATH%" "%SCRIPT_PATH_1%"
+
+if not exist "%SCRIPT_PATH_2%" (
+    echo Fehler: Skript nicht gefunden: %SCRIPT_PATH_2%
+    exit /B 1
+)
+
+echo Starte Skript: %SCRIPT_PATH_2%
+"%PYTHON_PATH%" "%SCRIPT_PATH_2%"
+
+echo.
+echo Die Skripte wurden ausgefuehrt. Druecke eine Taste, um zu beenden.
+pause
