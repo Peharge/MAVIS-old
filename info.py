c46bc16dc8a11d7c6577ba535a96c223bc9861e3
Peharge <Julian.kropff@gmx.de>
2024-12-08 19:56:59 +0100

    Update main python

--- info.py
+++ info.py
@@ -1,3 +1,45 @@
+# Englisch | Peharge: This source code is released under the MIT License.
+#
+# Usage Rights:
+# The source code may be copied, modified, and adapted to individual requirements.
+# Users are permitted to use this code in their own projects, both for private and commercial purposes.
+# However, it is recommended to modify the code only if you have sufficient programming knowledge,
+# as changes could cause unintended errors or security risks.
+#
+# Dependencies and Additional Frameworks:
+# The code relies on the use of various frameworks and executes additional files.
+# Some of these files may automatically install further dependencies required for functionality.
+# It is strongly recommended to perform installation and configuration in an isolated environment
+# (e.g., a virtual environment) to avoid potential conflicts with existing software installations.
+#
+# Disclaimer:
+# Use of the code is entirely at your own risk.
+# Peharge assumes no liability for damages, data loss, system errors, or other issues
+# that may arise directly or indirectly from the use, modification, or redistribution of the code.
+#
+# Please read the full terms of the MIT License to familiarize yourself with your rights and obligations.
+
+# Deutsch | Peharge: Dieser Quellcode wird unter der MIT-Lizenz veröffentlicht.
+#
+# Nutzungsrechte:
+# Der Quellcode darf kopiert, bearbeitet und an individuelle Anforderungen angepasst werden.
+# Nutzer sind berechtigt, diesen Code in eigenen Projekten zu verwenden, sowohl für private als auch kommerzielle Zwecke.
+# Es wird jedoch empfohlen, den Code nur dann anzupassen, wenn Sie über ausreichende Programmierkenntnisse verfügen,
+# da Änderungen unbeabsichtigte Fehler oder Sicherheitsrisiken verursachen könnten.
+#
+# Abhängigkeiten und zusätzliche Frameworks:
+# Der Code basiert auf der Nutzung verschiedener Frameworks und führt zusätzliche Dateien aus.
+# Einige dieser Dateien könnten automatisch weitere Abhängigkeiten installieren, die für die Funktionalität erforderlich sind.
+# Es wird dringend empfohlen, die Installation und Konfiguration in einer isolierten Umgebung (z. B. einer virtuellen Umgebung) durchzuführen,
+# um mögliche Konflikte mit bestehenden Softwareinstallationen zu vermeiden.
+#
+# Haftungsausschluss:
+# Die Nutzung des Codes erfolgt vollständig auf eigene Verantwortung.
+# Peharge übernimmt keinerlei Haftung für Schäden, Datenverluste, Systemfehler oder andere Probleme,
+# die direkt oder indirekt durch die Nutzung, Modifikation oder Weitergabe des Codes entstehen könnten.
+#
+# Bitte lesen Sie die vollständigen Lizenzbedingungen der MIT-Lizenz, um sich mit Ihren Rechten und Pflichten vertraut zu machen.
+
 import platform
 import psutil
 import os
