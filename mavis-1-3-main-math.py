2d1eff23561f4c534d0ef7b7eaba71d633bcbe06
Peharge <Julian.kropff@gmx.de>
2025-01-24 19:35:07 +0100

    Update

--- mavis-1-3-main-math.py
+++ mavis-1-3-main-math.py
@@ -150,7 +150,7 @@ def execute_python_code(md_content):
         return "---"
 
     # Verzeichnis für gespeicherte Bilder
-    image_dir = os.path.join(os.path.expanduser("~"), "PycharmProjects", "MAVIS", "main", "static", "image")
+    image_dir = os.path.join(os.path.expanduser("~"), "PycharmProjects", "MAVIS", "static", "image")
     if not os.path.exists(image_dir):
         os.makedirs(image_dir)
 
