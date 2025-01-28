bca0f7dbd6a44f18b7a8f47f7d054483fa1285bc
Peharge <Julian.kropff@gmx.de>
2025-01-28 20:15:02 +0100

    Update

--- info/info-start-mavis-1-5-math-ultra.py
+++ info/info-start-mavis-1-5-math-ultra.py
@@ -83,8 +83,13 @@ red = "\033[91m"
 green = "\033[92m"
 yellow = "\033[93m"
 blue = "\033[94m"
+magenta = "\033[95m"
+cyan = "\033[96m"
+white = "\033[97m"
+black = "\033[30m"
+orange = "\033[38;5;214m"
 reset = "\033[0m"
-
+bold = "\033[1m"
 # Farbiges Drucken des Textes
 print(f"Don't forget to update {blue}pip{reset} with {yellow}python -m pip install --upgrade pip{reset} every now and then\n")
 
--- install/install-mavis-1-5.py
+++ install/install-mavis-1-5.py
@@ -71,7 +71,13 @@ red = "\033[91m"
 green = "\033[92m"
 yellow = "\033[93m"
 blue = "\033[94m"
+magenta = "\033[95m"
+cyan = "\033[96m"
+white = "\033[97m"
+black = "\033[30m"
+orange = "\033[38;5;214m"
 reset = "\033[0m"
+bold = "\033[1m"
 
 def confirm_action(message: str) -> bool:
     """Fordert den Benutzer zur Bestätigung auf."""
