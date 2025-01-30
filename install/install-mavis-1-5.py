e10d218369c36acba7a6caa4db2c0905c35ddeda
Peharge <Julian.kropff@gmx.de>
2025-01-30 17:38:12 +0100

    Update

--- install/install-mavis-1-5.py
+++ install/install-mavis-1-5.py
@@ -141,7 +141,7 @@ packages = [
     "pyomo", "gekko", "casadi", "control", "pybullet", "h2o", "pint", "coolprop",
     "pythermo", "biopython", "opencv-python", "SimpleITK", "nilearn", "deepchem",
     "pymedtermino", "lifelines", "rdkit", "ase", "chempy", "shapely", "fiona", "cartopy",
-    "statsmodels", "yfinance", "QuantLib", "PySpice", "networkx", "schematics"
+    "statsmodels", "yfinance", "QuantLib", "pyspice", "networkx", "schematics"
 
 ]
 
