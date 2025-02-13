a715083afbfb9962129bcbf52b572e75d30f28f5
Peharge <Julian.kropff@gmx.de>
2025-02-13 18:56:53 +0100

    Update

--- test/test-main-draw.py
+++ test/test-main-draw.py
@@ -4,14 +4,16 @@ from flask import Flask, render_template, request, jsonify
 from PIL import Image
 from pix2tex.cli import LatexOCR
 import os
+import base64
+import io
 
 # Initialize Flask app
 app = Flask(__name__)
 
 # Directory to save uploaded images
-UPLOAD_FOLDER = 'uploads'
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+UPLOAD_FOLDER_latex = 'uploads'
+os.makedirs(UPLOAD_FOLDER_latex, exist_ok=True)
+app.config['UPLOAD_FOLDER_latex'] = UPLOAD_FOLDER_latex
 
 # Initialize the model
 model = LatexOCR()
@@ -20,22 +22,32 @@ model = LatexOCR()
 def index():
     return render_template('index.html')
 
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 400
-    file = request.files['file']
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'}), 400
+    if 'file' in request.files:
+        file = request.files['file']
+        if file.filename == '':
+            return jsonify({'error': 'No selected file'}), 400
 
-    # Save the uploaded image
-    image_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-    file.save(image_path)
+        # Save the uploaded image
+        image_path_latex = os.path.join(app.config['UPLOAD_FOLDE_latex'], file.filename)
+        file.save(image_path_latex)
+        img_latex = Image.open(image_path_latex)
+
+    elif 'image' in request.json:
+        # Handle Base64-encoded canvas image
+        image_latex_data = request.json['image']
+        image_latex_data = image_latex_data.split(",")[1]  # Entferne das Präfix 'data:image/png;base64,'
+        image_latex_bytes = base64.b64decode(image_latex_data)
+        img_latex = Image.open(io.BytesIO(image_latex_bytes))
+
+    else:
+        return jsonify({'error': 'No image data provided'}), 400
 
     # Process the image and get the LaTeX prediction
     try:
-        img = Image.open(image_path)
-        latex_code = model(img)
+        latex_code = model(img_latex)
         return jsonify({'latex': latex_code})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
