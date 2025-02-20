51e1f1d33eac79fc3b8734c33d2f6a66887a097d
Peharge <Julian.kropff@gmx.de>
2025-02-20 18:57:20 +0100

    Update

--- mavis-1-5-main-code.py
+++ mavis-1-5-main-code.py
@@ -355,7 +355,7 @@ def send_message():
         # Verarbeite die Nachricht ohne Bild, benutze das Standardbild
         try:
             response = ollama.chat(
-                model='deepseek-r1:14b',
+                model='qwen2.5-coder:14b',
                 messages=[{
                     'role': 'user',
                     'content': user_message
