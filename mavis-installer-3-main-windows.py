b5560ab651462901e3ca11a074725f4bedd37ae6
Peharge <Julian.kropff@gmx.de>
2025-03-16 19:26:30 +0100

    Update

--- mavis-installer-3-main-windows.py
+++ mavis-installer-3-main-windows.py
@@ -234,6 +234,7 @@ def display_versions():
         "mavis-3-code-fast": ("MAVIS fast start", f"With {red}Xc++ 3 11B{reset} or {blue}Llama3.2 11B{reset} + {blue}Qwen 2.5 Coder 14B{reset} + {blue}Qwen 2.5 1.5b{reset} + {blue}granite3.2-vision 2b{reset}", "qwen2.5-code:14b"),
         "mavis-3-3-main-fast": ("MAVIS fast start", f"With {red}Xc++ 3 11B{reset} or {blue}Llama3.2 11B{reset} + {blue}Gemma3 12B{reset} + {blue}Qwen 2.5 1.5b{reset} + {blue}granite3.2-vision 2b{reset}", "gemma3:12b"),
         "mavis-3-3-math-fast": ("MAVIS fast start", f"With {red}Xc++ 3 11B{reset} or {blue}Llama3.2 11B{reset} + {blue}QwQ 32b{reset} + {blue}Qwen 2.5 1.5b{reset} + {blue}granite3.2-vision 2b{reset}", "qwq"),
+        "mavis-terminal-3": ("MAVIS Terminal 3 EAP", "The MAVIS Terminal is always available for you!!!", "")
     }
 
     print(f"All MAVIS versions are available here:\n\n{green}█{reset} Required LLM model for this MAVIS version is already installed\n{orange}█{reset} Required LLM model for this MAVIS version is not yet installed\n{blue}█{reset} LLM model is available for you - you have all the permissions\n{red}█{reset} LLM model is not available for you - you do not have permission to install the model")
@@ -254,7 +255,6 @@ def display_versions():
 
 def get_user_input(versions):
     """Fragt nach einer MAVIS-Batch-Datei und führt sie aus."""
-    print(f"\n4. MAVIS Terminal 3 EAP\n   - {green}mavis-terminal-3{reset}: The MAVIS Terminal is always available for you!!!")
     while True:
         user_input = input("\nEnter a MAVIS batch file (e.g. 'mavis-3-code'):").strip()
         if user_input in versions:
