492df2529e22eee6f8b3902d51392e3bcef30011
Peharge <Julian.kropff@gmx.de>
2025-03-28 19:17:51 +0100

    Update

--- mavis-terminal-4.py
+++ mavis-terminal-4.py
@@ -445,15 +445,18 @@ def handle_special_commands(user_input):
         "m ls": "mavis-terminal\\m-ls.py", # new
         "models ls": "mavis-terminal\\models-ls.py",  # new
         "m models ls": "mavis-terminal\\m-models-ls.py",  # new
-        "m github": "mavis-terminal\\m-github-mavis.py",  # new
         "m github mavis": "mavis-terminal\\m-github-mavis.py",  # new
         "m github commits": "mavis-terminal\\m-github-commits.py",  # new
         "m github issues": "mavis-terminal\\m-github-issues.py",  # new
         "m github peharge": "mavis-terminal\\m-github-peharge.py",  # new
         "m github pulls": "mavis-terminal\\m-github-pulls.py",  # new
         "m github readme": "mavis-terminal\\m-github-readme.py",  # new
-        "m github releases": "mavis-terminal\\m-github-releases.py"  # new
-
+        "m github releases": "mavis-terminal\\m-github-releases.py",  # new
+        "m github": "mavis-terminal\\m-github.py",  # new
+        "m search": "mavis-terminal\\m-search.py",  # new
+        "m google": "mavis-terminal\\m-google.py",  # new
+        "m ollama": "mavis-terminal\\m-ollama.py",  # new
+        "m huggingface": "mavis-terminal\\m-huggingface.py"  # new
     }
 
     if user_input in commands:
