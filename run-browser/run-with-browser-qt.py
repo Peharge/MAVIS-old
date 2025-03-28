17ecfd0749648256ae631e3636da14a51f27ebb3
Peharge <Julian.kropff@gmx.de>
2025-03-28 17:40:34 +0100

    Update

--- run-browser/run-with-browser-qt.py
+++ run-browser/run-with-browser-qt.py
@@ -187,7 +187,7 @@ class BrowserWindow(QMainWindow):
         super().__init__()
 
         # Setzt den Titel und die Größe des Fensters
-        self.setWindowTitle("MAVIS GitHub Repository")
+        self.setWindowTitle("MAVIS QT Browser")
         self.setGeometry(100, 100, 1024, 768)
 
         user = os.getenv("USERNAME") or os.getenv("USER")
