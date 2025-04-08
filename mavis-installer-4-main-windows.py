8cfc016fcff4cf06adcab776373f998f8e9cffdd
Peharge <Julian.kropff@gmx.de>
2025-04-08 19:45:49 +0200

    Update

--- mavis-installer-4-main-windows.py
+++ mavis-installer-4-main-windows.py
@@ -234,9 +234,9 @@ def display_versions():
         "mavis-3-code-fast": ("MAVIS fast start", f"With {red}Xc++ 3 11B{reset} or {blue}Llama3.2 11B{reset} + {blue}Qwen 2.5 Coder 14B{reset} + {blue}Qwen 2.5 1.5b{reset} + {blue}granite3.2-vision 2b{reset}", "qwen2.5-code:14b"),
         "mavis-3-3-main-fast": ("MAVIS fast start", f"With {red}Xc++ 3 11B{reset} or {blue}Llama3.2 11B{reset} + {blue}Gemma3 12B{reset} + {blue}Qwen 2.5 1.5b{reset} + {blue}granite3.2-vision 2b{reset}", "gemma3:12b"),
         "mavis-3-3-math-fast": ("MAVIS fast start", f"With {red}Xc++ 3 11B{reset} or {blue}Llama3.2 11B{reset} + {blue}QwQ 32b{reset} + {blue}Qwen 2.5 1.5b{reset} + {blue}granite3.2-vision 2b{reset}", "qwq"),
-        "mavis-terminal-4": ("MAVIS Terminal 3 EAP", "The MAVIS Terminal is always available for you!!!", ""),
+        "mavis-terminal-4": ("MAVIS Terminal 4 EAP", "The MAVIS Terminal is always available for you!!!", ""),
         "mavis-4": ("MAVIS 4 EAP", "NEW - Development of MAVIS 4 has begun – featuring new Vision Models, a more powerful and faster MAVIS Terminal, and access to over 200 models.", ""),
-        "mavis-terminal-4": ("MAVIS Terminal 4 EAP", "The MAVIS Terminal is always available for you!!!", "")
+        "mavis-terminal-5": ("MAVIS Terminal 5 EAP", "The MAVIS Terminal is always available for you!!!", "")
     }
 
     print(f"All MAVIS versions are available here:\n\n{green}█{reset} Required LLM model for this MAVIS version is already installed\n{orange}█{reset} Required LLM model for this MAVIS version is not yet installed\n{blue}█{reset} LLM model is available for you - you have all the permissions\n{red}█{reset} LLM model is not available for you - you do not have permission to install the model")
