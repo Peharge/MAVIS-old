ef2cb7e45bf25164c1be554e3edc883ac7c963a1
Peharge <Julian.kropff@gmx.de>
2025-04-11 19:13:22 +0200

    Update

--- mavis-terminal-5.py
+++ mavis-terminal-5.py
@@ -2288,7 +2288,7 @@ def main():
                     run_ubuntu_command(user_input)
 
             elif user_input.startswith("ubuntu-c "):
-                user_input = user_input[7:].strip()
+                user_input = user_input[9:].strip()
                 if not is_wsl_installed():
                     print("WSL is not installed or could not be found. Please install WSL to use this feature.")
                 else:
